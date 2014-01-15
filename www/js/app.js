49cd76f0e015b791a8c9eb6ad1d8e063c92adf25
francho <lab@francho.org>
2014-01-15 10:17:33 +0100

    fix scope issues in callback functions

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 var myApp = angular.module('MyApp', ['ngTouch', 'ngRoute']);
 
@@ -19,7 +19,6 @@ myApp.service('CordovaService', ['$document', '$q',
 		var d = $q.defer(),
 			resolved = false;
 
-		var self = this;
 		this.ready = d.promise;
 
 		document.addEventListener('deviceready', function () {
@@ -37,79 +36,76 @@ myApp.service('CordovaService', ['$document', '$q',
 	}]);
 
 myApp.controller('MainController', function ($scope, CordovaService) {
+	$scope.cordovaReady = false;
+	$scope.photos = [];
+	$scope.locationWatchId = null;
+	$scope.positions = [];
+
 	CordovaService.ready.then(function () {
 		$scope.cordovaReady = true;
-
-		$scope.myAlert = function () {
-			var phoneModel = device.model;
-			alert(phoneModel);
-		};
-
-
-		$scope.myNotification = function () {
-
-			function callback() {
-				console.log('alert dimissed');
-			}
-
-			navigator.notification.alert(
-				'It works!',  // message
-				callback,         // callback
-				'3...2...1',            // title
-				':)'                  // buttonName
-			);
-
-			var milliseconds = 1000;
-			navigator.notification.vibrate(milliseconds);
-		};
-
-		$scope.photos = [];
-		$scope.myTakePhoto = function () {
-			function captureSuccess(mediaFiles) {
-				for (var i = 0, len = mediaFiles.length; i < len; i += 1) {
-					$scope.photos.push( mediaFiles[i].fullPath);
-					console.log("new photo");
-				}
-			}
-			var options = { limit: 1 };
-			navigator.device.capture.captureImage(captureSuccess, this.onError, options);
-		};
-
-
-
-		$scope.locationWatchId = null;
-
-		$scope.myLocation = function () {
-			function onLocationSuccess(position) {
-				$scope.myPosition += 'Latitude: ' + position.coords.latitude + '<br />' +
-					'Longitude: ' + position.coords.longitude + '<br />' +
-					'<hr />';
-			}
-
-			function startWatchLocation() {
-				var options = { timeout: 30000 };
-				$scope.locationWatchId = navigator.geolocation.watchPosition(onLocationSuccess, this.onError, options);
-				console.log("locationWatchId=" + this.locationWatchId);
-			}
-
-			function stopWatchLocation() {
-				navigator.geolocation.clearWatch(this.locationWatchId);
-				$scope.locationWatchId = null;
-			}
-
-			console.log('current locationWatchId = ' + this.locationWatchId);
-			if (this.locationWatchId == null) {
-				startWatchLocation.call(this);
-			} else {
-				stopWatchLocation.call(this);
-			}
-
-		};
-
-		function onError(error) {
-			alert('code: ' + error.code + '\n' +
-				'message: ' + error.message + '\n');
-		};
-
 	});
+
+	$scope.myAlert = function () {
+		var phoneModel = device.model;
+		alert(phoneModel);
+	};
+
+	$scope.myNotification = function () {
+		function callback() {
+			console.log('alert dimissed');
+		}
+
+		navigator.notification.alert(
+			'It works!',  // message
+			callback,         // callback
+			'3...2...1',            // title
+			':)'                  // buttonName
+		);
+
+		var milliseconds = 1000;
+		navigator.notification.vibrate(milliseconds);
+	};
+
+	$scope.myTakePhoto = function () {
+		function captureSuccess(mediaFiles) {
+			for (var i = 0, len = mediaFiles.length; i < len; i += 1) {
+				var fullPath = mediaFiles[i].fullPath;
+				$scope.photos.push(fullPath);
+			}
+			$scope.$digest();
+		}
+
+		var options = { limit: 1 };
+		navigator.device.capture.captureImage(captureSuccess, onError, options);
+	};
+
+	$scope.myLocation = function () {
+		function onLocationSuccess(position) {
+			$scope.positions.unshift(position.coords);
+			$scope.$digest();
+		}
+
+		function startWatchLocation() {
+			var options = { timeout: 30000 };
+			$scope.locationWatchId = navigator.geolocation.watchPosition(onLocationSuccess, onError, options);
+		}
+
+		function stopWatchLocation() {
+			navigator.geolocation.clearWatch($scope.locationWatchId);
+			$scope.locationWatchId = null;
+		}
+
+		if (this.locationWatchId == null) {
+			startWatchLocation.call(this);
+		} else {
+			stopWatchLocation.call(this);
+		}
+
+	};
+
+	function onError(error) {
+		alert('code: ' + error.code + '\n' +
+			'message: ' + error.message + '\n');
+	}
+
 });
